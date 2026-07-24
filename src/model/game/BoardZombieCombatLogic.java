a2a6d845bc8cc1e27784fab75e7c0a92c7faeddb
Reza <rezaghazavi2007@gmail.com>
2026-07-24 13:41:17 +0330

    split game logic into smaller classses

---
+++ src/model/game/BoardZombieCombatLogic.java
@@ -0,0 +1,402 @@
+package model.game;
+
+import model.game.entities.EntityPosition;
+import model.game.entities.other.Sun;
+import model.game.entities.plants.BasePlant;
+import model.game.entities.plants.modifier.Modifier;
+import model.game.entities.plants.wallnut.Wallnut;
+import model.game.entities.zombies.Zombie;
+import model.game.entities.zombies.abilities.FlyAbility;
+import model.game.entities.zombies.abilities.FastSwimAbility;
+import model.game.entities.zombies.abilities.FishingHookAbility;
+import model.game.entities.zombies.abilities.KingBuffAbility;
+import model.game.entities.zombies.abilities.PianoCrushAbility;
+import model.game.entities.zombies.abilities.SmashAbility;
+import model.game.entities.zombies.abilities.SubmergeAbility;
+import model.game.entities.zombies.abilities.SurfAbility;
+import model.game.entities.zombies.abilities.TackleAbility;
+import model.game.entities.zombies.abilities.TorchAbility;
+import model.game.entities.zombies.abilities.WizardSpellAbility;
+import model.game.entities.zombies.abilities.ZombieAbility;
+import view.game.ZombieView;
+
+abstract class BoardZombieCombatLogic extends BoardZombieMovementLogic {
+    protected BoardZombieCombatLogic() {
+        super();
+    }
+
+    protected BoardZombieCombatLogic(int numberOfRows, int numberOfColumns) {
+        super(numberOfRows, numberOfColumns);
+    }
+
+    void updateZombie(Zombie zombie, float deltaSeconds) {
+        finishDodoFlight(zombie);
+        if (keepStationaryZombieAtRightEdge(zombie)) {
+            return;
+        }
+        BasePlant blockingPlant = findNearestPlantAhead(zombie);
+        if (tryTransformPlantWithWizard(
+                zombie, blockingPlant)) {
+            return;
+        }
+        updateBeachMovementStates(zombie, blockingPlant);
+        Zombie blockingHypnotizedZombie = findNearestHypnotizedZombieAhead(zombie);
+        if (blockingPlant != null
+                && (blockingHypnotizedZombie == null
+                || blockingHypnotizedZombie.getColumnPosition()
+                <= blockingPlant.getEntityPosition().getColumn())
+                && tryCrushPlantWithPiano(zombie, blockingPlant)) {
+            reportDestroyedPlant(blockingPlant);
+            return;
+        }
+        if (blockingPlant != null
+                && (blockingHypnotizedZombie == null
+                || blockingHypnotizedZombie.getColumnPosition()
+                <= blockingPlant.getEntityPosition().getColumn())
+                && tryCrushPlantWithSurfboard(zombie, blockingPlant)) {
+            reportDestroyedPlant(blockingPlant);
+            return;
+        }
+        if (blockingPlant != null
+                && (blockingHypnotizedZombie == null
+                || blockingHypnotizedZombie.getColumnPosition()
+                <= blockingPlant.getEntityPosition().getColumn())
+                && tryFlyOverPlant(zombie, blockingPlant)) {
+            return;
+        }
+        if (blockingPlant != null
+                && (blockingHypnotizedZombie == null
+                || blockingHypnotizedZombie.getColumnPosition()
+                <= blockingPlant.getEntityPosition().getColumn())
+                && tryBurnPlant(zombie, blockingPlant)) {
+            reportDestroyedPlant(blockingPlant);
+            return;
+        }
+        if (blockingHypnotizedZombie != null
+                && (blockingPlant == null
+                || blockingHypnotizedZombie.getColumnPosition()
+                > blockingPlant.getEntityPosition().getColumn())) {
+            updateZombieAgainstHypnotizedTarget(zombie,
+                    blockingHypnotizedZombie, deltaSeconds);
+            return;
+        }
+        if (blockingPlant == null) {
+            zombie.move(deltaSeconds, 0.0);
+            if (zombie.getColumnPosition() <= POSITION_EPSILON) {
+                zombie.markReachedHouse();
+            }
+            return;
+        }
+
+        double attackColumn = blockingPlant.getEntityPosition().getColumn() + Zombie.ATTACK_REACH;
+        if (zombie.getColumnPosition() <= attackColumn + POSITION_EPSILON) {
+            if (tryTacklePlant(zombie, blockingPlant)) {
+                reportDestroyedPlant(blockingPlant);
+                return;
+            }
+            attackPlant(zombie, blockingPlant, deltaSeconds);
+            handleWallnutAfterAttack(zombie, blockingPlant, deltaSeconds);
+            reportDestroyedPlant(blockingPlant);
+        } else {
+            zombie.move(deltaSeconds, attackColumn);
+        }
+    }
+
+    boolean tryCrushPlantWithPiano(
+            Zombie zombie, BasePlant plant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (!(ability instanceof PianoCrushAbility)) {
+                continue;
+            }
+            PianoCrushAbility piano =
+                    (PianoCrushAbility) ability;
+            if (!piano.canCrush(zombie, plant)) {
+                continue;
+            }
+            plant.takeDamage(Integer.MAX_VALUE);
+            pendingResults.add(zombie.getName()
+                    + " crushed " + plant.getName()
+                    + " with its piano at "
+                    + plant.getEntityPosition() + ".");
+            return true;
+        }
+        return false;
+    }
+
+    boolean tryTransformPlantWithWizard(
+            Zombie zombie, BasePlant plant) {
+        if (plant == null
+                || plant.getEntityPosition() == null) {
+            return false;
+        }
+
+        double distance = zombie.getColumnPosition()
+                - plant.getEntityPosition().getColumn();
+        if (distance < 0.0
+                || distance > Zombie.ATTACK_REACH
+                        + POSITION_EPSILON) {
+            return false;
+        }
+
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (!(ability instanceof WizardSpellAbility)) {
+                continue;
+            }
+            WizardSpellAbility spell =
+                    (WizardSpellAbility) ability;
+            boolean transformed =
+                    spell.transformReachedPlant(
+                            zombie, plant);
+            if (transformed) {
+                pendingResults.add(zombie.getName()
+                        + " transformed "
+                        + plant.getName()
+                        + " into a cat on contact.");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    boolean keepStationaryZombieAtRightEdge(
+            Zombie zombie) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof FishingHookAbility
+                    || ability instanceof KingBuffAbility) {
+                zombie.moveTo(numberOfColumns - 1.0);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void updateBeachMovementStates(Zombie zombie,
+            BasePlant blockingPlant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof SubmergeAbility) {
+                ((SubmergeAbility) ability).updateState(
+                        zombie, asBoard(), blockingPlant);
+            } else if (ability instanceof FastSwimAbility) {
+                ability.tryUse(zombie, asBoard());
+            } else if (ability instanceof SurfAbility) {
+                ability.tryUse(zombie, asBoard());
+            }
+        }
+    }
+
+    boolean tryCrushPlantWithSurfboard(Zombie zombie,
+            BasePlant plant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (!(ability instanceof SurfAbility)) {
+                continue;
+            }
+            SurfAbility surf = (SurfAbility) ability;
+            if (!surf.tryCrush(zombie, plant, asBoard())) {
+                continue;
+            }
+            plant.takeDamage(Integer.MAX_VALUE);
+            pendingResults.add(zombie.getName() + " crushed "
+                    + plant.getName() + " with its surfboard at "
+                    + plant.getEntityPosition() + ".");
+            return true;
+        }
+        return false;
+    }
+
+    boolean tryFlyOverPlant(Zombie zombie, BasePlant plant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof FlyAbility
+                    && ((FlyAbility) ability).tryFlyOver(zombie, plant, asBoard())) {
+                pendingResults.add(zombie.getName() + " flew over "
+                        + plant.getName() + ".");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void finishDodoFlight(Zombie zombie) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof FlyAbility
+                    && ((FlyAbility) ability).isFlying()) {
+                ((FlyAbility) ability).finishFlight(zombie);
+            }
+        }
+    }
+
+    boolean tryBurnPlant(Zombie zombie, BasePlant plant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (!(ability instanceof TorchAbility)) {
+                continue;
+            }
+            TorchAbility torch = (TorchAbility) ability;
+            if (!torch.canBurn(zombie, plant)
+                    || !torch.tryUse(zombie, asBoard())) {
+                continue;
+            }
+            plant.takeDamage(Integer.MAX_VALUE);
+            pendingResults.add(zombie.getName() + " burned "
+                    + plant.getName() + " at " + plant.getEntityPosition() + ".");
+            return true;
+        }
+        return false;
+    }
+
+    boolean tryTacklePlant(Zombie zombie, BasePlant plant) {
+        TackleAbility tackle = useTackle(zombie);
+        if (tackle == null) {
+            return false;
+        }
+        int lethalDamage = Math.max(tackle.getSmashDamage(), plant.getCurrentHP());
+        plant.takeDamage(lethalDamage);
+        pendingResults.add(zombie.getName() + " tackled and destroyed "
+                + plant.getName() + " at " + plant.getEntityPosition() + ".");
+        return true;
+    }
+
+    boolean tryTackleZombie(Zombie zombie, Zombie target) {
+        TackleAbility tackle = useTackle(zombie);
+        if (tackle == null) {
+            return false;
+        }
+        int lethalDamage = Math.max(tackle.getSmashDamage(), target.getHitPoints());
+        target.takeDirectDamage(lethalDamage);
+        pendingResults.add(zombie.getName() + " tackled and destroyed hypnotized "
+                + target.getName() + ".");
+        return true;
+    }
+
+    TackleAbility useTackle(Zombie zombie) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof TackleAbility
+                    && ability.tryUse(zombie, asBoard())) {
+                return (TackleAbility) ability;
+            }
+        }
+        return null;
+    }
+
+    void attackPlant(Zombie zombie, BasePlant plant, float deltaSeconds) {
+        boolean eaten = !trySmashPlant(zombie, plant);
+        if (eaten) {
+            zombie.eat(plant, deltaSeconds);
+        }
+        handleModifierAfterAttack(zombie, plant, eaten);
+    }
+
+    void handleModifierAfterAttack(Zombie zombie, BasePlant plant,
+            boolean eaten) {
+        if (!eaten || !(plant instanceof Modifier)) {
+            return;
+        }
+        Modifier modifier = (Modifier) plant;
+        if (modifier.onEatenBy(zombie)) {
+            pendingResults.add("Zombie " + zombie.getName()
+                    + " was hypnotized by " + modifier.getName() + ".");
+        }
+    }
+
+    boolean trySmashPlant(Zombie zombie, BasePlant plant) {
+        for (ZombieAbility ability : zombie.getAbilities()) {
+            if (ability instanceof SmashAbility && ability.tryUse(zombie, asBoard())) {
+                plant.takeDamage(Integer.MAX_VALUE);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void handleWallnutAfterAttack(Zombie zombie, BasePlant plant, float deltaSeconds) {
+        if (!(plant instanceof Wallnut)) {
+            return;
+        }
+        Wallnut wallnut = (Wallnut) plant;
+        reflectEndurianDamage(zombie, wallnut, deltaSeconds);
+        divertZombieAfterGarlicBite(zombie, wallnut);
+        releaseSunBeanSun(wallnut);
+        applyWallnutExplosion(wallnut);
+        if (zombie.isDead()) {
+            reportZombieDeath(zombie);
+        }
+    }
+
+    void reflectEndurianDamage(Zombie zombie, Wallnut wallnut, float deltaSeconds) {
+        int reflectedDamage = wallnut.calculateReflectedDamage(deltaSeconds);
+        if (reflectedDamage > 0) {
+            damageZombieOrFrozenShell(
+                    zombie, reflectedDamage, false);
+        }
+    }
+
+    void divertZombieAfterGarlicBite(Zombie zombie, Wallnut wallnut) {
+        if (!wallnut.drainDivertLanePending()) {
+            return;
+        }
+        int targetLane = wallnut.chooseAdjacentLane(zombie.getLane(), numberOfRows);
+        zombie.moveToLane(targetLane);
+        pendingResults.add("Garlic diverted " + zombie.getName() + " into lane " + targetLane + ".");
+    }
+
+    void releaseSunBeanSun(Wallnut wallnut) {
+        int sunAmount = wallnut.drainPendingSunAmount();
+        if (sunAmount <= 0) {
+            return;
+        }
+        addEntity(Sun.createPlantSun(sunAmount, wallnut.getEntityPosition()));
+        pendingResults.add("plant Sun Bean produced " + sunAmount + " sun at "
+                + wallnut.getEntityPosition());
+    }
+
+    void applyWallnutExplosion(Wallnut wallnut) {
+        int explosionDamage = wallnut.drainPendingExplosionDamage();
+        if (explosionDamage <= 0 || wallnut.getEntityPosition() == null) {
+            return;
+        }
+        EntityPosition center = wallnut.getEntityPosition();
+        for (Zombie target : getZombies()) {
+            if (!target.isHypnotized()
+                    && !target.isSubmerged()
+                    && isInsideThreeByThree(target, center)) {
+                target.takeDamage(explosionDamage);
+            }
+        }
+        pendingResults.add(wallnut.getName() + " exploded for " + explosionDamage
+                + " damage around " + center + ".");
+    }
+
+    BasePlant findNearestPlantAhead(Zombie zombie) {
+        BasePlant nearestPlant = null;
+        int nearestColumn = -1;
+        for (BasePlant plant : getPlants()) {
+            if (plant.isRemoved()
+                    || plant.isTransformedToSheep()
+                    || plant.getEntityPosition().getRow()
+                            != zombie.getLane()) {
+                continue;
+            }
+            int plantColumn = plant.getEntityPosition().getColumn();
+            if (plantColumn <= zombie.getColumnPosition() + POSITION_EPSILON
+                    && isBetterBlocker(plant, nearestPlant, plantColumn, nearestColumn)) {
+                nearestPlant = plant;
+                nearestColumn = plantColumn;
+            }
+        }
+        return nearestPlant;
+    }
+
+    void reportDestroyedPlant(BasePlant plant) {
+        if (plant.isDestroyed()) {
+            pendingResults.add("Plant " + plant.getName() + " at "
+                    + plant.getEntityPosition() + " is destroyed.");
+        }
+    }
+
+    void reportZombieDeath(Zombie zombie) {
+        if (zombie.isDeathReported()) {
+            return;
+        }
+        zombie.markDeathReported();
+        zombie.markForRemoval();
+        pendingResults.add(ZombieView.buildDeathMessage(zombie));
+    }
+}
---
+++ src/model/game/GameSpecialLevelLogic.java
@@ -0,0 +1,227 @@
+package model.game;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import model.game.entities.EntityPosition;
+import model.game.gameTypes.GameType;
+import model.game.special.DeadLineSystem;
+import model.game.special.LoveYourPlantsSystem;
+import model.game.special.PlantWhatYouGetSystem;
+import model.game.special.ProtectedPlantSpec;
+import model.game.special.ProtectedPlantStatus;
+import model.game.special.SaveOurSeedsSystem;
+import model.game.special.TimedWarObjective;
+import model.game.special.TimedWarSystem;
+
+abstract class GameSpecialLevelLogic extends GameLoadoutLogic {
+    protected GameSpecialLevelLogic(Board board, GameType gameType,
+            int initialSunCount, List<ZombieWave> zombieWaves,
+            Random random, boolean startWavesImmediately,
+            ChapterRuleset chapterRuleset, int difficultyLevel) {
+        super(board, gameType, initialSunCount, zombieWaves, random,
+                startWavesImmediately, chapterRuleset, difficultyLevel);
+    }
+
+    public void enableSaveOurSeeds(
+            List<ProtectedPlantSpec> protectedPlants) {
+        if (saveOurSeedsSystem != null) {
+            throw new IllegalStateException(
+                    "Save Our Seeds is already enabled");
+        }
+        saveOurSeedsSystem =
+                new SaveOurSeedsSystem(
+                        board, protectedPlants);
+        pendingResults.addAll(
+                saveOurSeedsSystem.getStartMessages());
+    }
+
+    public boolean hasSaveOurSeeds() {
+        return saveOurSeedsSystem != null;
+    }
+
+    public List<ProtectedPlantStatus>
+            getProtectedPlantStatuses() {
+        if (saveOurSeedsSystem == null) {
+            return Collections.emptyList();
+        }
+        return saveOurSeedsSystem.getStatuses(board);
+    }
+
+    public boolean isProtectedSeedAt(
+            EntityPosition position) {
+        return saveOurSeedsSystem != null
+                && saveOurSeedsSystem
+                        .isProtectedPlantAt(position);
+    }
+
+    ProtectedPlantStatus
+            getFailedProtectedPlant() {
+        if (saveOurSeedsSystem == null) {
+            return null;
+        }
+        return saveOurSeedsSystem
+                .findFailedPlant(board);
+    }
+
+    public void enableTimedWarZombieKills(
+            double durationSeconds,
+            int requiredKills) {
+        timedWarSystem = TimedWarSystem.forZombieKills(
+                durationSeconds, requiredKills);
+        pendingResults.add(
+                "Timed War started: "
+                        + timedWarSystem.describeObjective()
+                        + ".");
+    }
+
+    public void enableTimedWarSunProduction(
+            double durationSeconds,
+            int requiredSun) {
+        timedWarSystem =
+                TimedWarSystem.forSunProduction(
+                        durationSeconds, requiredSun);
+        pendingResults.add(
+                "Timed War started: "
+                        + timedWarSystem.describeObjective()
+                        + ".");
+    }
+
+    public boolean hasTimedWar() {
+        return timedWarSystem != null;
+    }
+
+    public TimedWarObjective getTimedWarObjective() {
+        return timedWarSystem == null
+                ? null : timedWarSystem.getObjective();
+    }
+
+    public int getTimedWarProgress() {
+        return timedWarSystem == null
+                ? 0 : timedWarSystem.getProgress();
+    }
+
+    public int getTimedWarTarget() {
+        return timedWarSystem == null
+                ? 0 : timedWarSystem.getTarget();
+    }
+
+    public double getTimedWarRemainingSeconds() {
+        return timedWarSystem == null
+                ? 0.0
+                : timedWarSystem.getRemainingSeconds();
+    }
+
+    public void enableNightOps() {
+        disableSkySuns("Night Ops");
+        pendingResults.add(
+                "Night Ops started: no sun will fall from the sky.");
+    }
+
+    public void disableSkySuns(String reason) {
+        skySunsDisabled = true;
+        skySunDisabledReason =
+                reason == null ? "" : reason;
+    }
+
+    public boolean areSkySunsDisabled() {
+        return skySunsDisabled;
+    }
+
+    public String getSkySunDisabledReason() {
+        return skySunDisabledReason;
+    }
+
+    public void enableDeadLine(double lineColumn) {
+        deadLineSystem =
+                new DeadLineSystem(lineColumn);
+        pendingResults.add(
+                "Dead Line active at column "
+                        + String.format(
+                                Locale.ROOT, "%.1f",
+                                lineColumn)
+                        + ".");
+    }
+
+    public boolean hasDeadLine() {
+        return deadLineSystem != null;
+    }
+
+    public double getDeadLineColumn() {
+        return deadLineSystem == null
+                ? -1.0
+                : deadLineSystem.getLineColumn();
+    }
+
+    public void enableLoveYourPlants(
+            int maximumLostPlants) {
+        loveYourPlantsSystem =
+                new LoveYourPlantsSystem(
+                        maximumLostPlants);
+        loveYourPlantsSystem.observePlants(board);
+        pendingResults.add(
+                "Love Your Plants started: lose the level "
+                        + "after losing "
+                        + maximumLostPlants + " plants.");
+    }
+
+    public boolean hasLoveYourPlants() {
+        return loveYourPlantsSystem != null;
+    }
+
+    public int getLostPlantCount() {
+        return loveYourPlantsSystem == null
+                ? 0
+                : loveYourPlantsSystem
+                        .getLostPlantCount();
+    }
+
+    public int getMaximumLostPlants() {
+        return loveYourPlantsSystem == null
+                ? 0
+                : loveYourPlantsSystem
+                        .getMaximumLostPlants();
+    }
+
+    public void enablePlantWhatYouGet() {
+        plantWhatYouGetSystem =
+                new PlantWhatYouGetSystem();
+        disableSkySuns("Plant What You Get");
+        pendingResults.add(
+                "Plant What You Get setup started: "
+                        + "sun-producing plants are locked, "
+                        + "cooldowns are ignored until "
+                        + "start zombie waves.");
+    }
+
+    public boolean hasPlantWhatYouGet() {
+        return plantWhatYouGetSystem != null;
+    }
+
+    protected final void beginZombieWaves() {
+        if (zombieWavesStarted) {
+            return;
+        }
+        zombieWavesStarted = true;
+        startNextWaveIfPossible();
+    }
+
+    public boolean startZombieWaves() {
+        if (plantWhatYouGetSystem == null
+                || !plantWhatYouGetSystem
+                        .startZombieWaves()) {
+            return false;
+        }
+        zombieWavesStarted = true;
+        startNextWaveIfPossible();
+        pendingResults.add(
+                "Zombie waves started; recharge cooldowns "
+                        + "are active now.");
+        return true;
+    }
+
+    public boolean haveZombieWavesStarted() {
+        return zombieWavesStarted;
+    }
+}
